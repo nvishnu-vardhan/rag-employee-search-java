64a95706862d1304a78742b16e05c3a340b27c6d
vishnuvn116 <120576108+nvishnu-vardhan@users.noreply.github.com>
2025-11-25 00:18:13 +0530

    Add Spring Boot main application class

---
+++ backend/src/main/java/com/vishnu/employee/EmployeeSearchApplication.java
@@ -0,0 +1,11 @@
+package com.vishnu.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSearchApplication.class, args);
+    }
+}
